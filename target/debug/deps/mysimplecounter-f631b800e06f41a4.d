e8f5c5cb3f0baf56b67b85aa56db76a5ab8207f2
datacryptoanalytics <datacryptoanalytics@protonmail.com>
2020-10-03 10:06:51 -0400

    debugs

--- target/debug/deps/mysimplecounter-f631b800e06f41a4.d
+++ target/debug/deps/mysimplecounter-f631b800e06f41a4.d
@@ -1,6 +1,6 @@
-/home/felipe/Site/simple-secret-contracts/mysimplecounter/target/debug/deps/mysimplecounter-f631b800e06f41a4.rmeta: src/lib.rs src/contract.rs src/msg.rs src/state.rs
+/home/felipe/Site/secret-network-test/target/debug/deps/mysimplecounter-f631b800e06f41a4.rmeta: src/lib.rs src/contract.rs src/msg.rs src/state.rs
 
-/home/felipe/Site/simple-secret-contracts/mysimplecounter/target/debug/deps/mysimplecounter-f631b800e06f41a4.d: src/lib.rs src/contract.rs src/msg.rs src/state.rs
+/home/felipe/Site/secret-network-test/target/debug/deps/mysimplecounter-f631b800e06f41a4.d: src/lib.rs src/contract.rs src/msg.rs src/state.rs
 
 src/lib.rs:
 src/contract.rs:
